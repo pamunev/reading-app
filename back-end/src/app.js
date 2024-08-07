83537d004ea4c1fc4d64e1562a6fad6bbf20d4d0
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-08-07 00:57:32 -0500

    Initial commit front-end. Server built in back-end.

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 app.use(express.json());
 app.use(cors());
+
+app.get((req, res) => {
+  res.json("Hello!");
+});
 
 module.exports = app;
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,7 @@
-const { PORT = 5001 } = process.env.PORT;
+const { PORT = 5001 } = process.env;
 
 const app = require("./app");
 
 app.listen(PORT, () => {
-  console.log(`Listening here, baby: ${PORT}`);
+  console.log(`Listening here, baby: http://localhost:${PORT}/`);
 });
